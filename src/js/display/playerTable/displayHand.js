e50e11ca81ecf2181c2ee2a7c82ae1e15758a809
Kateryna Tytova <titova.kat.85@gmail.com>
2021-01-05 11:12:52 +0200

    feat: add hand ui css

--- src/js/display/playerTable/displayHand.js
+++ src/js/display/playerTable/displayHand.js
@@ -17,7 +17,7 @@ const displayHand = {
     this.arrowTop.innerHTML = /* html */ `
     <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
       xmlns:a="http://ns.adobe.com/AdobeSVGViewerExtensions/3.0/"
-      x="0px" y="0px" width="100px" height="80px" viewBox="0 0 213.7 213.7" enable-background="new 0 0 213.7 213.7"
+      x="0px" y="0px" width="50px" height="50px" viewBox="0 0 230 213.7" enable-background="new 0 0 230 213.7"
       xml:space="preserve">
 
       <polygon class='hand__controls-svg--triangle' id="XMLID_18_" fill="none" stroke-width="7" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="
@@ -35,7 +35,7 @@ const displayHand = {
     this.arrowBottom.innerHTML = /* html */ `
     <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
       xmlns:a="http://ns.adobe.com/AdobeSVGViewerExtensions/3.0/"
-      x="0px" y="0px" width="100px" height="80px" viewBox="0 0 213.7 213.7" enable-background="new 0 0 213.7 213.7"
+      x="0px" y="0px" width="50px" height="50px" viewBox="0 0 230 213.7" enable-background="new 0 0 230 213.7"
       xml:space="preserve">
 
       <polygon class='hand__controls-svg--triangle' id="XMLID_18_" fill="none" stroke-width="7" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="
