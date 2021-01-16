c08f21ebfae92c4bc0913edf0edb7e10eb6cfbb8
IgorAleks88 <igor.alexeyenko@gmail.com>
2021-01-16 15:02:48 +0200

    edit age8 cards in json

--- src/js/components/gameState.js
+++ src/js/components/gameState.js
@@ -22,7 +22,7 @@ const gameState = {
     actionPoints: 0,
     hand: [],
     currentAge: 1,
-    currentDeck: 'age9', // test
+    currentDeck: 'age8', // test
     activeDecks: {
       red: {
         cards: [],
