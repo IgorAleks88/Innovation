670e139c43965818239eca1fb33dfa1f6d1d176e
IgorAleks88 <igor.alexeyenko@gmail.com>
2021-01-29 15:32:09 +0200

    work commit

--- src/js/components/tutorial.js
+++ src/js/components/tutorial.js
@@ -11,6 +11,10 @@ const tutorial = {
       displayModal.setMessageText('На столе справа расположены карты, которые доступны для всех игроков: колоды эпох, лидерства и особые карты');
       tutorial.currentDOMElement = document.querySelector('.aside');
       tutorial.currentDOMElement.style.zIndex = 120;
+      // remove onclick event from active decks
+      document.querySelectorAll('.age-deck--active').forEach((e) => {
+        e.onclick = '';
+      });
       displayModal.modalBtn.onclick = tutorial.clickFunctions.stage3;
     },
     stage3: () => {
@@ -18,7 +22,14 @@ const tutorial = {
       displayModal.setMessageText('Внизу распологаются карты в руке игрока');
       tutorial.currentDOMElement = document.querySelector('.hand');
       tutorial.currentDOMElement.style.zIndex = 120;
-      // displayModal.modalBtn.onclick = tutorial.clickFunctions.stage3;
+      displayModal.modalBtn.onclick = tutorial.clickFunctions.stage4;
+    },
+    stage4: () => {
+      tutorial.currentDOMElement.style.zIndex = 0;
+      displayModal.setMessageText('По центру распологаются карты в активной зоне игрока');
+      tutorial.currentDOMElement = document.querySelector('.active-zone');
+      tutorial.currentDOMElement.style.zIndex = 120;
+      displayModal.modalBtn.onclick = tutorial.clickFunctions.stage4;
     },
   },
 
