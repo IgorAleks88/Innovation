b0dd9348d62259332c102bcf1bb45e181081654d
Kateryna Tytova <titova.kat.85@gmail.com>
2021-01-31 17:49:32 +0200

    feat: add modal window stile, add modal window drag-n-drop

--- src/js/components/dogmaModal.js
+++ src/js/components/dogmaModal.js
@@ -2,6 +2,7 @@ function dogmaModalMessages(text, playerName, isAggressive, qa) {
   return new Promise((resolve) => {
     const modal = document.createElement('div');
     modal.classList.add('modal__turn-step');
+    modal.setAttribute('draggable', 'true');
     modal.innerHTML = /* html */`
     <div class="message"><b>${playerName}</b>: ${text}</div>
       <div class="container__message"></div>
@@ -12,6 +13,34 @@ function dogmaModalMessages(text, playerName, isAggressive, qa) {
     `;
     const messages = modal.querySelector('.container__message');
     const btnWrapper = modal.querySelector('.wrapper__btn');
+
+    // add drag-n-drop for modal window
+    function dragStart(event) {
+      const style = window.getComputedStyle(event.target, null);
+      event.dataTransfer.setData('text/plain',
+        `${parseInt(style.getPropertyValue('left'), 10) - event.clientX},${parseInt(style.getPropertyValue('top'), 10) - event.clientY}`);
+    }
+
+    function dragEnter(event) {
+      event.preventDefault();
+      return true;
+    }
+    function dragOver(event) {
+      event.preventDefault();
+      return false;
+    }
+    function dragDrop(event) {
+      const offset = event.dataTransfer.getData('text/plain').split(',');
+      modal.style.left = `${event.clientX + parseInt(offset[0], 10)}px`;
+      modal.style.top = `${event.clientY + parseInt(offset[1], 10)}px`;
+      event.preventDefault();
+      return false;
+    }
+
+    modal.addEventListener('dragstart', dragStart);
+    document.body.addEventListener('dragenter', dragEnter);
+    document.body.addEventListener('dragover', dragOver);
+    document.body.addEventListener('drop', dragDrop);
 
     btnWrapper.addEventListener('click', (e) => {
       if (e.target === btnWrapper) return;
